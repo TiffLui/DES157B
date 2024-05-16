72499a543f9de1d8b98625bb9b083b26da629167
Tiffany <tplui@ucdavis.edu>
2024-05-16 03:22:08 -0700

    added studio 3 and linked to portal

--- studio3/script.js
+++ studio3/script.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
 
-    //Granim 
+    // Granim 
     const granimInstance = new Granim({
         element: '#gradientContainer',
         direction: 'top-bottom',
@@ -16,14 +16,30 @@
             }
         }
     });
-    
-    //jqPuzzle
-    $(document).ready(function() { 
-        $('img').jqPuzzle(); // apply to all images
-        const settings = {
-            rows: 3,
-            cols: 3,
-            hole: 9
-        } 
-    }); 
+
+    // Instructions
+    // When the Game Loads
+    document.addEventListener('DOMContentLoaded', function() {
+        // Show the overlay when the page loads
+        const overlay = document.getElementById('instructions');
+        if (overlay) {
+            overlay.style.display = 'flex';
+        } else {
+            console.error('Overlay element not found');
+        }
+    });
+    // Existing JavaScript code...
+
+// Function to close the instructions popup
+function closeInstructionsPopup() {
+    const instructions = document.getElementById('instructions');
+    if (instructions) {
+        instructions.style.display = 'none'; // Hides the popup
+    }
+}
+
+    // Event listener for the "Start" button
+    document.getElementById('startBtn').addEventListener('click', closeInstructionsPopup);
+
+
 }());
